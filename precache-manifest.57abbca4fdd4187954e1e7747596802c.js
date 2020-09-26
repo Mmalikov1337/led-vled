e133bc42237280a44623daa1025eee6279ab8671
Maksim Malikov <grelexis1337@inbox.ru>
2020-09-26 16:20:22 +0600

    Updates

---
+++ precache-manifest.57abbca4fdd4187954e1e7747596802c.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc752a7ccdb95b99d0663e493527bc70",
+    "url": "/led-vled/index.html"
+  },
+  {
+    "revision": "9519d0c3ebfe79ef24d2",
+    "url": "/led-vled/static/css/main.ec448573.chunk.css"
+  },
+  {
+    "revision": "e1e6dae3ec79ce0cd2a7",
+    "url": "/led-vled/static/js/2.41112890.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/led-vled/static/js/2.41112890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9519d0c3ebfe79ef24d2",
+    "url": "/led-vled/static/js/main.6e0da091.chunk.js"
+  },
+  {
+    "revision": "31e8f2e3749f7f84e7d0",
+    "url": "/led-vled/static/js/runtime-main.936a022f.js"
+  },
+  {
+    "revision": "c065d9cc7e94087828e3011ff1673456",
+    "url": "/led-vled/static/media/1_1.c065d9cc.png"
+  },
+  {
+    "revision": "e3fc1fd0f977b30d4b972319c454c21c",
+    "url": "/led-vled/static/media/1_2.e3fc1fd0.png"
+  },
+  {
+    "revision": "850d11227455475d2aca9802e37ee9df",
+    "url": "/led-vled/static/media/1_3.850d1122.png"
+  },
+  {
+    "revision": "b0315c66ac84b5a960f2d7aa6980634b",
+    "url": "/led-vled/static/media/1_4.b0315c66.png"
+  },
+  {
+    "revision": "fc6f51b202764144958f712744771e68",
+    "url": "/led-vled/static/media/1_5.fc6f51b2.png"
+  },
+  {
+    "revision": "2c679836fb396779e411030288d95650",
+    "url": "/led-vled/static/media/CeraRoundPro-Black.2c679836.woff"
+  },
+  {
+    "revision": "542ac8a19035aab4f64618a0b4d0d901",
+    "url": "/led-vled/static/media/CeraRoundPro-Black.542ac8a1.woff2"
+  },
+  {
+    "revision": "7db56f4787bd940520e85c6dda111d92",
+    "url": "/led-vled/static/media/CeraRoundPro-Black.7db56f47.eot"
+  },
+  {
+    "revision": "e312ec59e2278931488beba2d3326cfc",
+    "url": "/led-vled/static/media/CeraRoundPro-Black.e312ec59.ttf"
+  },
+  {
+    "revision": "7375729f04d15ea2fd94cc77ee6c3977",
+    "url": "/led-vled/static/media/CeraRoundPro-Bold.7375729f.ttf"
+  },
+  {
+    "revision": "cee2b123dd6fd5378f68ff7685bcd8a1",
+    "url": "/led-vled/static/media/CeraRoundPro-Bold.cee2b123.woff2"
+  },
+  {
+    "revision": "f3fa6e56987f2ce29e49ea3e9b14ebbc",
+    "url": "/led-vled/static/media/CeraRoundPro-Bold.f3fa6e56.eot"
+  },
+  {
+    "revision": "f57761c26477b123ea333ae63ce79eeb",
+    "url": "/led-vled/static/media/CeraRoundPro-Bold.f57761c2.woff"
+  },
+  {
+    "revision": "4ed3604efc55b5c55f61e04d3e26f5ed",
+    "url": "/led-vled/static/media/CeraRoundPro-Light.4ed3604e.woff"
+  },
+  {
+    "revision": "6f509b1aa72c6a5aac3207d9b60299c6",
+    "url": "/led-vled/static/media/CeraRoundPro-Light.6f509b1a.ttf"
+  },
+  {
+    "revision": "ba5f23173e15ea2b69617e30f2b42f48",
+    "url": "/led-vled/static/media/CeraRoundPro-Light.ba5f2317.woff2"
+  },
+  {
+    "revision": "e215174ef15abaa0e1f6e34671d982cc",
+    "url": "/led-vled/static/media/CeraRoundPro-Light.e215174e.eot"
+  },
+  {
+    "revision": "7c56cbc15d0c3c537b92efd02da3686b",
+    "url": "/led-vled/static/media/CeraRoundPro-Medium.7c56cbc1.woff"
+  },
+  {
+    "revision": "89f9302e5ab628f1c2b09d1c707b2b87",
+    "url": "/led-vled/static/media/CeraRoundPro-Medium.89f9302e.woff2"
+  },
+  {
+    "revision": "9e8131d61b480aa0212d1ef96e05428f",
+    "url": "/led-vled/static/media/CeraRoundPro-Medium.9e8131d6.eot"
+  },
+  {
+    "revision": "da7af710e16dd1fabedcf78cbc61b619",
+    "url": "/led-vled/static/media/CeraRoundPro-Medium.da7af710.ttf"
+  },
+  {
+    "revision": "0f8f52f8a9d3435749b9226fd8fd1ba5",
+    "url": "/led-vled/static/media/CeraRoundPro-Regular.0f8f52f8.eot"
+  },
+  {
+    "revision": "78bebe3d94484c93e24967f0b86e5c4f",
+    "url": "/led-vled/static/media/CeraRoundPro-Regular.78bebe3d.woff2"
+  },
+  {
+    "revision": "aa065c6b03e2eb4a6580dcd93385618a",
+    "url": "/led-vled/static/media/CeraRoundPro-Regular.aa065c6b.ttf"
+  },
+  {
+    "revision": "da039a931cf0684488d4ce1ea0dbe18f",
+    "url": "/led-vled/static/media/CeraRoundPro-Regular.da039a93.woff"
+  },
+  {
+    "revision": "0ad6b32f5cb7c5068b4e1e6cee8ede53",
+    "url": "/led-vled/static/media/CeraRoundPro-Thin.0ad6b32f.woff2"
+  },
+  {
+    "revision": "0e8d35e32641bc3bf1e0f7b7345568e0",
+    "url": "/led-vled/static/media/CeraRoundPro-Thin.0e8d35e3.woff"
+  },
+  {
+    "revision": "3139696b5a20d3cc135fc5c7a8042a01",
+    "url": "/led-vled/static/media/CeraRoundPro-Thin.3139696b.ttf"
+  },
+  {
+    "revision": "de1732b709b82349201c0c244636b75b",
+    "url": "/led-vled/static/media/CeraRoundPro-Thin.de1732b7.eot"
+  },
+  {
+    "revision": "8670fe3d136351c24b6c9ce845b7bbdf",
+    "url": "/led-vled/static/media/apple.8670fe3d.svg"
+  },
+  {
+    "revision": "3bcf704f36d77ef1a4ea53ad982c013d",
+    "url": "/led-vled/static/media/arrow_down.3bcf704f.svg"
+  },
+  {
+    "revision": "22d9f42da83226fec603719a0737ec72",
+    "url": "/led-vled/static/media/cart.22d9f42d.svg"
+  },
+  {
+    "revision": "a767f98b052d1c3eb274cc07319794aa",
+    "url": "/led-vled/static/media/germany.a767f98b.svg"
+  },
+  {
+    "revision": "fe9dd07fb4428c9cd0ffb1cfe3814f2e",
+    "url": "/led-vled/static/media/icecream.fe9dd07f.svg"
+  },
+  {
+    "revision": "194a4579000d3444c9fac0073071a861",
+    "url": "/led-vled/static/media/kcal.194a4579.svg"
+  },
+  {
+    "revision": "23606cfc77eb38b00ea89c0a5d59d25a",
+    "url": "/led-vled/static/media/logo.23606cfc.svg"
+  },
+  {
+    "revision": "d2e76b6d951f8aa53b9867b846ce5f12",
+    "url": "/led-vled/static/media/logo_white.d2e76b6d.svg"
+  },
+  {
+    "revision": "8988189cb71d29fc05e2ffddf44198f8",
+    "url": "/led-vled/static/media/peach.8988189c.png"
+  },
+  {
+    "revision": "508469c2966ee5395b1f43877f2ccf31",
+    "url": "/led-vled/static/media/phone.508469c2.svg"
+  },
+  {
+    "revision": "6ee650e60705fad524a045c40b0220a9",
+    "url": "/led-vled/static/media/shadow.6ee650e6.png"
+  },
+  {
+    "revision": "354a26df81adb0d655a7fecbb2234c11",
+    "url": "/led-vled/static/media/snow-flake-start-page.354a26df.svg"
+  },
+  {
+    "revision": "b0f478f9245661abcec105c6e4933295",
+    "url": "/led-vled/static/media/star_full.b0f478f9.svg"
+  },
+  {
+    "revision": "8866e551b283afa8cca2afe660af9b89",
+    "url": "/led-vled/static/media/usage_pic.8866e551.png"
+  }
+]);
