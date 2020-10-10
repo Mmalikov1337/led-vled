0d6c18683eccdd8a06c25af5a3ac2df7cc682004
Maksim Malikov <grelexis1337@inbox.ru>
2020-10-10 23:19:34 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/led-vled/precache-manifest.7201f1fe6d8ab6635064493d78b6d68a.js"
+  "/led-vled/precache-manifest.4f4ac144382df9f1b3576e5a448e451d.js"
 );
 
 self.addEventListener('message', (event) => {
